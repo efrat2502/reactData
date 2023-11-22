a9b353404c67aa9f8e5540aa87801d076f29f1ea
EfratElimelech2502 <efratelimelech@hilma.tech>
2023-11-22 22:16:13 +0200

    postt work with comments but doesnt work with slice

--- src/components/ChosenPost.jsx
+++ src/components/ChosenPost.jsx
@@ -2,13 +2,21 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 const ChosenPost = () => {
+  const [endNum, setEndNum] = useState(4);
+  const [startNum, setStartNum] = useState(0);
   const [post, setPost] = useState({});
+  const [showComments, setShowComments] = useState(false);
+  const [comments, setComments] = useState([]);
+
   const { postId } = useParams();
   console.log("postId: ", postId);
+
+  // get chosen post from db
   const getPost = async () => {
     try {
-      // if (user.id) {
-      const response = await fetch(` http://localhost:3000/posts?id=${postId}`);
+      const response = await fetch(
+        `http://localhost:3000/posts/${postId}?_embed=comments`
+      );
       if (!response.ok) {
         throw "error";
       } else {
@@ -17,23 +25,49 @@ const ChosenPost = () => {
         console.log(resPost);
       }
     } catch (error) {
-      // }
       console.error("Error fetching posts:", error);
     }
   };
+
+  // reset comments when the post changes
+  useEffect(() => {
+    setComments([]);
+  }, [post]);
+
   useEffect(() => {
     getPost();
-  }, []);
-  function showComments() {}
+  }, [showComments]);
 
   return (
     <>
       {post && (
         <div>
-          <span>{post[0]?.id}</span>
-          <h2>{post[0]?.title}</h2>
-          <p>{post[0]?.body}</p>
-          <button onClick={showComments}>comments</button>
+          <span>{post?.id}</span>
+          <h2>{post?.title}</h2>
+          <p>{post?.body}</p>
+          <button onClick={() => setShowComments((prev) => !prev)}>💬</button>
+          {showComments && (
+            <>
+              {post.comments.map((comment) => (
+                <div
+                  style={{ backgroundColor: "powderblue", margin: "10px" }}
+                  key={comment.id}
+                >
+                  <span>{comment.email}</span>
+                  <h3>{comment.name}</h3>
+                  <p>{comment.body}</p>
+                </div>
+              ))}
+              <button
+                onClick={() => {
+                  setStartNum((prev) => prev + endNum);
+                  setEndNum((prev) => prev + 10);
+                }}
+              >
+                show more
+              </button>
+            </>
+          )}
         </div>
       )}
     </>
