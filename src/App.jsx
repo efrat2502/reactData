f4a50d3bb3128d06a31bd211e180a378acaae87d
EfratElimelech2502 <efratelimelech@hilma.tech>
2023-11-22 10:10:52 +0200

    routs

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
-import SignUp from "./SignUp";
+import SignUp from "./components/SignUp";
+import Login from "./components/Login";
+import Home from "./components/Home";
+import Posts from "./components/Posts";
+import Albums from "./components/Albums";
+import Todos from "./components/Todos";
+import Layout from "./components/Layout";
+import Welcome from "./components/Welcome";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,28 +16,18 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/books">Books</Link>
-            </li>
-          </ul>
-        </nav>
-
-        {/* <Routes>
-        <Route path="/" element={<LogIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/about" element={< />}>
-          <Route index element={< />} />
-          <Route path=":id" element={< />} />
-        </Route>
-      </Routes> */}
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/users" element={<Layout />}>
+            <Route path="home" element={<Home />} />
+            <Route path="posts" element={<Posts />} />
+            <Route path="albums" element={<Albums />} />
+            <Route path="todos" element={<Todos />} />
+            {/* <Route path="*" element={<NoPage />} /> */}
+          </Route>
+        </Routes>
       </BrowserRouter>
     </>
   );
