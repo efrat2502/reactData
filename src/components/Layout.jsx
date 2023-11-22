0d392d74bcc1d7e1079776b169ad62afd546e7df
EfratElimelech2502 <efratelimelech@hilma.tech>
2023-11-22 10:34:33 +0200

    router works

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,22 +1,27 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link, Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
     <>
+      <button>info</button>
       <nav>
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <Link to="./home">Home</Link>
           </li>
           <li>
-            <Link to="/albums">Albums</Link>
+            <Link to="./albums">Albums</Link>
           </li>
           <li>
-            <Link to="/posts">Posts</Link>
+            <Link to="./posts">Posts</Link>
+          </li>
+          <li>
+            <Link to="./todos">Todos</Link>
           </li>
         </ul>
       </nav>
+      <Outlet />
     </>
   );
 };
